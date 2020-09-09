8ddc91c4a018a56b85b12e222947f0d6d1125e9c
Walid <alioua.walid1993@gmail.com>
2020-09-09 12:47:16 +0100

    corrected

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap/dist/js/bootstrap.js';
 
 
 function App() {
